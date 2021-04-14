56e09230b8fbc2faae508f0c4c73f61bf1a673c6
Hyena-Feet <82405199+Hyena-Feet@users.noreply.github.com>
2021-04-14 13:45:59 -0400

    Added Sprite

--- main.ts
+++ main.ts
@@ -2,28 +2,13 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setPosition(randint(0, 160), randint(0, 120))
     mySprite2.setPosition(randint(0, 160), randint(0, 120))
     mySprite3.setPosition(randint(0, 160), randint(0, 120))
+    mySprite4.setPosition(randint(0, 160), randint(0, 120))
 })
+let mySprite4: Sprite = null
 let mySprite3: Sprite = null
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(assets.image`Rex`, SpriteKind.Player)
 mySprite2 = sprites.create(assets.image`Pencil`, SpriteKind.Player)
 mySprite3 = sprites.create(assets.image`Dav`, SpriteKind.Player)
-let mySprite4 = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+mySprite4 = sprites.create(assets.image`Fisjing_Guy`, SpriteKind.Player)
